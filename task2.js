400a906044a642db02748d44d6849d5d26ba8559
squirell-programmer <sirenvictory@gmail.com>
2018-06-13 23:51:05 +0300

    Decision of task #2 (Fibonacci).

---
+++ task2.js
@@ -0,0 +1,30 @@
+function check_params(param) {
+    if (param < -10000 || param > 10000) {
+        process.stderr.write('Input param is incorrect');
+    }
+}
+
+function read_params() {
+    result = Number(process.argv[2]);
+    check_params(result);
+    return result;
+}
+
+function fib(n) {
+    if (n < 0) {
+        return NaN;
+    }
+    var a = 1, b = 1;
+    for (i = 3; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+function main() {
+    process.stdout.write(String(fib(read_params())));
+}
+
+main();

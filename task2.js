b93534ae09b9916084ffd4eeae415b20e7bdcced
squirell-programmer <sirenvictory@gmail.com>
2018-06-16 10:59:14 +0300

    Improvements of Fibonacci function

--- task2.js
+++ task2.js
@@ -11,6 +11,7 @@ function read_params() {
 }
 
 function fib(n) {
+    // work correctly only upto fib(78)!!!
     if (n < 0) {
         return NaN;
     } else if (n == 0) {
@@ -25,8 +26,48 @@ function fib(n) {
     return b;
 }
 
+function sum_for_two_nearest (x, y) {
+
+    var max_length = Math.max(x.length, y.length) - 1;
+    var result = [], buf = 0;
+
+    // alignment of arrays
+    for (var i=0; i <= max_length - y.length; i++) { y.push(0) }
+    for (var i=0; i <= max_length - x.length; i++) { x.push(0) }
+
+    for (var i=0; i <= max_length; i++) {
+        tmp = x[i] + y[i] + buf
+        result[i] = tmp % 10
+        buf = Math.floor(tmp / 10)
+    }
+
+    if (buf > 0) {
+        result.push(buf)
+    }
+
+    return result
+}
+
+function fib_for_biggest(n) {
+    if (n < 0) {
+        return NaN;
+    } else if (n == 0) {
+        return 0;
+    }
+
+    var a = [1], b = [1];
+    for (i = 3; i <= n; i++) {
+        c = sum_for_two_nearest(a, b);
+        a = b;
+        b = c;
+    }
+
+    result = b.reverse().join('')
+    return result;
+}
+
 function main() {
-    process.stdout.write(String(fib(read_params())));
+    process.stdout.write(String(fib_for_biggest(read_params())));
 }
 
 main();
